ab341ac66d701ad4834082d266454515f03302e5
muhammadzainiy31 <muhammadzainiy31@gmail.com>
2021-06-05 21:39:52 +0800

    Initial commit

---
+++ src/main.java
@@ -0,0 +1,14 @@
+public class main {
+    public static void main(String[] args){
+        String namaDepan = "Muhammad";
+        String namaTengah ="Zaini";
+        String namaLengkap ="MuhammadZaini";
+        int usia = 22;
+        int targetTahunKuliah = 4;
+        double ipk = 3.89764512;
+        char nilaiAbjab ='A';
+        boolean tampan = true;
+
+        System.out.println(namaDepan + namaTengah + namaLengkap);
+    }
+}
